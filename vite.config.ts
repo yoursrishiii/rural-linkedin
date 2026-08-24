851a15bdc0235fa554aaca7651e5a688e9fbe913
yoursrishiii <rishi.yadav1.ug25@iilm.edu>
2026-08-24 09:40:40 +0530

    Publish working GramWork app

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,8 @@ import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 import { metaImagesPlugin } from "./vite-plugin-meta-images";
 
 export default defineConfig({
-  base: process.env.GITHUB_ACTIONS === "true" ? "/rural-linkedin/" : "/",
+  base:
+    process.env.GITHUB_ACTIONS === "true" ? "/rural-linkedin/" : "/",
   plugins: [
     react(),
     runtimeErrorOverlay(),
